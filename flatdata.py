029c05f709c84c28b70beee518f8595175c01ee8
Louis Liss <lliss@mit.edu>
2018-04-07 16:30:24 -0400

    update

--- flatdata.py
+++ flatdata.py
@@ -7,4 +7,8 @@ os.chdir("Data")
 climate = pd.read_csv("climateriskbytract.csv", index_col=0)
 hmda = pd.read_csv("hmda_data.csv", index_col=0, low_memory=False)
 
-hmda
+hmda.shape
+
+hmdaclimate = pd.merge(hmda, climate, how='left',on='census_tract_number')
+
+hmdaclimate.to_csv("hmdaclimate.csv")
